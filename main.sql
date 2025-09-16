9502c6810dd1b6e30c925a11ad32ed523e6abfbf
SattapriusCamel <oikkoandfriends90@gmail.com>
2025-09-16 20:46:29 +0200

    capstone project final

--- main.sql
+++ main.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Salesman (
- Salesman_id REAL PRIMARY KEY
+ Salesman_id REAL PRIMARY KEY,
  NAME TEXT,
  CITY TEXT,
  COMISSION REAL 
@@ -16,7 +16,7 @@ INSERT INTO Salesman (Salesman_id,NAME,CITY,COMISSION) VALUES
  (197, 'Ali', 'Tehran', 0.0313);
 
 CREATE TABLE IF NOT EXISTS Customers (
- customer_id REAL PRIMARY KEY
+ customer_id REAL PRIMARY KEY,
  NAME TEXT,
  CITY TEXT,
  GRADE TEXT,
@@ -50,5 +50,52 @@ INSERT INTO Orders (ord_no, purch_amt, ord_date, customer_id, Salesman_id) VALUE
  (1005, 66.67, '01-04-2014', 63, 194),
  (1006, 19.84, '19-08-2014', 61, 193);
 
+SELECT * FROM Salesman;
+SELECT * FROM Customers;
+SELECT * FROM Orders;
 
 
+
+SELECT Customers.NAME, Salesman.NAME, Salesman.city
+FROM Customers
+JOIN Salesman ON Customers.city = Salesman.city;
+
+SELECT Customers.NAME, Salesman.NAME 
+FROM Customers
+JOIN Salesman ON Customers.Salesman_id = Salesman.Salesman_id;
+
+SELECT Orders.ord_no, Customers.NAME, Salesman.NAME
+FROM Orders
+JOIN Customers ON Orders.customer_id = Customers.customer_id
+JOIN Salesman ON Orders.Salesman_id = Salesman.Salesman_id
+WHERE Customers.city <> Salesman.city;
+
+SELECT Orders.ord_no, Customers.NAME
+FROM Orders
+JOIN Customers ON Orders.customer_id = Customers.customer_id;
+
+SELECT Customers.NAME AS "Name", Customers.GRADE AS "Grade"
+FROM Orders
+JOIN Salesman ON Orders.Salesman_id = Salesman.Salesman_id
+JOIN Customers ON Orders.customer_id = Customers.customer_id
+WHERE Customers.GRADE IS NOT NULL;
+
+SELECT Customers.NAME AS "Customer",
+Customers.city AS "City",
+Salesman.NAME AS "Salesman",
+Salesman.COMISSION
+FROM Customers
+JOIN Salesman ON Customers.Salesman_id = Salesman.Salesman_id
+WHERE Salesman.COMISSION BETWEEN 0.12 AND 0.14;
+
+SELECT Orders.ord_no, Customers.NAME, Salesman.COMISSION AS "COMISSION%",
+Orders.purch_amt * Salesman.COMISSION AS "COMISSION"
+FROM Orders
+JOIN Salesman ON Orders.Salesman_id = Salesman.Salesman_id
+JOIN Customers ON Orders.customer_id = Customers.customer_id
+WHERE Customers.GRADE >= 200;
+
+SELECT *
+FROM Customers
+JOIN Orders ON Customers.customer_id = Orders.customer_id
+WHERE Orders.ord_date = '19-03-2014';
